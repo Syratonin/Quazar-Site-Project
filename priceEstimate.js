a760d4a1b056004de66b1ea3e0c4cfb081ce1027
Syratonin <thomasjasso10.9@gmail.com>
2025-12-12 18:37:17 -0700

    Delete priceEstimate.js

--- priceEstimate.js
+++
@@ -1,8 +0,0 @@
-function priceEstimate() {
-
-    let distance = document.getElementById("distance").value;
-
-    let price = distance * 0.20;
-
-    document.getElementById("priceDisplay").innerText = "Price estimate: $" + price;
-}
